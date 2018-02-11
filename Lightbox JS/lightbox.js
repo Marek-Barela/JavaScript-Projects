8505d63f49259cc82dc32b6c9304ffa0145c6e5b
Marek Bareła <marek.barela@wp.pl>
2018-02-11 17:38:32 +0100

    Lightbox commited

---
+++ Lightbox JS/lightbox.js
@@ -0,0 +1,28 @@
+const lightboxContent = document.querySelectorAll('.lightbox-img img')[0],
+      lightboxImages = document.querySelectorAll('.lightbox-images img');
+(function lightboxSlide() {
+    lightboxImages.forEach(function(img){
+        img.addEventListener('click', function(e){
+            if (lightboxContent.src == e.target.src) {
+                return -1
+            }
+            lightboxReset()
+            lighboxFadeIn()
+            lightboxContent.src = e.target.src
+            e.target.style.opacity = 0.4      
+        })
+    })
+})();
+function lightboxReset() {
+    lightboxImages.forEach(function(img){
+        img.style.opacity = 1
+    })
+};
+function lighboxFadeIn() {
+    lightboxImages.forEach(function(img){
+        lightboxContent.classList.add('lightbox-img')
+        setTimeout(function(){
+            lightboxContent.classList.remove('lightbox-img')
+        }, 300) 
+    })
+};
