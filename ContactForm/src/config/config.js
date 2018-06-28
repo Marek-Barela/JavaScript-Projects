6acb20c8b26dd8c2f2ba986be0fe26d08126db42
Marek <30760012+Marek-Barela@users.noreply.github.com>
2018-06-28 22:50:58 +0200

    basic layout commited

---
+++ ContactForm/src/config/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyAt2A_ge98X__XNezmGxaEpG3huSYv3HSU",
+    authDomain: "contactform-6acd7.firebaseapp.com",
+    databaseURL: "https://contactform-6acd7.firebaseio.com",
+    projectId: "contactform-6acd7",
+    storageBucket: "contactform-6acd7.appspot.com",
+    messagingSenderId: "484843761955"
+  };
---
+++ ContactForm/src/index.js
@@ -0,0 +1,5 @@
+import "./sass/style.sass";
+import { config } from "./config/config";
+import firebase from "firebase";
+
+firebase.initializeApp(config);
